029547855587e1072460869e6fd4f55e2889f7c0
guidomora <guidomorabito161@hotmail.com>
2025-03-03 17:02:22 -0300

    first commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,86 @@
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common/dto/pagintation.dto';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger('Products services')
+
+  onModuleInit() {
+    this.$connect()
+    this.logger.log('db');
+
+  }
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({
+      data: createProductDto
+    })
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto
+
+    const totalProducts = await this.product.count({where:{available:true}})
+    const lastPage = Math.ceil(totalProducts / limit)
+
+    const productPagination = await this.product.findMany({
+      take: limit,
+      skip: (page - 1) * limit,
+      where:{available:true}
+    })
+
+    if (productPagination.length === 0) return "The page does not exists"
+
+    return {
+      data: productPagination,
+      meta: {
+        page,
+        total: totalProducts,
+        lastPage
+      }
+    }
+  }
+
+  async findOne(id: number) {
+
+    const product = await this.product.findFirst({
+      where: {
+        id
+      }
+    })
+
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`)
+    }
+    return product
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    // this makes a double call. also can be done with a trycatch
+    await this.findOne(id)
+
+    return this.product.update({
+      where: { id, available:true },
+      data: updateProductDto
+    })
+  }
+
+  async remove(id: number) {
+    // this makes a double call. also can be done with a trycatch
+    await this.findOne(id)
+
+    // return this.product.delete({
+    //   where: {
+    //     id
+    //   }
+    // })
+
+    return this.product.update({
+      where: { id },
+      data: { available: false }
+    })
+  }
+}
